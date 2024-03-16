f962aa9660b06bf6061ecc2cdc79ca9f9c75e75b
Reynella Liebig <reynella.liebig@gmail.com>
2024-03-16 15:01:19 -0700

    testing CORS

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,55 @@ package main
 
 import (
 	"fmt"
-	"log"
+	// "log"
+	"encoding/json"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type Article struct {
+	Id      string
+	Title   string
+	Desc    string
+	Content string
 }
 
+var Articles []Article
+
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	println("Listening on http://localhost:8002/articles")
+	Articles = []Article{
+		{Id: "1", Title: "First article", Desc: "Title of this fine article", Content: "Content for this fine article"},
+		{Id: "2", Title: "Second article", Desc: "Title of this majestic article", Content: "Content for this majestic article"},
+	}
+	http.HandleFunc("/articles", handleArticles)
+	http.ListenAndServe(":8002", nil)
+}
+
+// func foo(w http.ResponseWriter, r *http.Request) {
+
+// 	js, err := json.Marshal(Articles)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Write(js)
+// }
+
+func enableCors(w *http.ResponseWriter) {
+	fmt.Println("Enabling CORS")
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+}
+
+func handleArticles(w http.ResponseWriter, r *http.Request) {
+
+	enableCors(&w)
+	js, err := json.Marshal(Articles)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }

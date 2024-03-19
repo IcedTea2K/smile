6eaa520d57b1fa34992a55072264206c28689e50
Minh Au <auduongminh2004@gmail.com>
2024-03-18 22:59:45 -0400

    capture video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const stream = useCaptureVideo()
+
+  useEffect(() => {
+    const peerConnection = new RTCPeerConnection 
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay 
+      ref={video => {
+        if (video)
+          video.srcObject = stream
+      }}/>
+      <h1> Happy </h1>
     </div>
   );
+}
+
+function useCaptureVideo() {
+  const [stream, setStream] = useState(null)
+
+  const captureVideo = async () => {
+    if (stream)
+      return
+    try {
+      setStream(await navigator.mediaDevices.getUserMedia({video: true})) 
+    } catch (error) {
+      console.log(error) 
+    }
+  }
+
+  captureVideo() // capture video without a way to stop
+
+  return stream
 }
 
 export default App;
